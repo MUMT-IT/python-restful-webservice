76285faf11be89b0e26a57c5c6afbc7979260fac
Likit Preeyanon <likit.pre@mahidol.edu>
2016-08-01 14:38:47 +0000

    fixded some errors

--- pop_item_code.sql
+++ pop_item_code.sql
@@ -1,6 +1,6 @@
 INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-001-0001', '7710', '001', '0001');
-INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-001-0001', '7710', '001', '0002');
-INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-001-0001', '7710', '002', '0001');
-INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-001-0001', '7710', '002', '0002');
+INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-001-0002', '7710', '001', '0002');
+INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-002-0001', '7710', '002', '0001');
+INSERT INTO item_code(display_code,group_class,type,description) VALUES('7710-002-0002', '7710', '002', '0002');
 INSERT INTO item_code(display_code,group_class,type,description) VALUES('7711-001-0001', '7711', '001', '0001');
 INSERT INTO item_code(display_code,group_class,type,description) VALUES('7711-002-0001', '7711', '002', '0001');
