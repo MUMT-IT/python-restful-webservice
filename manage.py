9afa9afba3f499a9bb99834b14f9ebdf935de605
Likit Preeyanon <likit.pre@mahidol.edu>
2016-08-02 18:17:07 +0700

    replace deprecated lib name with a new one

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app, db
-from flask.ext.script import Manager, Shell
+from flask_script import Manager, Shell
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
