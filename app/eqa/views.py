e8d2a67b967f2a1c8f5f34ea45de93c7cc74d105
Likit Preeyanon <likit.pre@mahidol.edu>
2016-08-02 23:54:51 +0700

    Changed url

--- app/eqa/views.py
+++ app/eqa/views.py
@@ -2,7 +2,7 @@ from flask import url_for, jsonify
 from . import eqa
 from .. import db
 
-@eqa.route('/schemes/api/0.1/<int:item_id>')
-@eqa.route('/schemes/api/0.1/')
+@eqa.route('/api/0.1/schemes/<int:item_id>')
+@eqa.route('/api/0.1/schemes/')
 def list_schemes(scheme_id=None):
     return jsonify({'sname': 'EQAI'})
