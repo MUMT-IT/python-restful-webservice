11470a32bf3f26ffeda5951732b05578c730cdd1
Likit Preeyanon <likit.pre@mahidol.edu>
2016-08-02 17:48:21 +0700

    Added models file

---
+++ model.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Role(db.Model):
+    __tablename__= 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.email
